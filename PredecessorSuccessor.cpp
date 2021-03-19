c1d5b90b2b6f14178ed458c5f1e6f388f880f3a6
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-03-20 00:20:41 +0530

    gfg

---
+++ PredecessorSuccessor.cpp
@@ -0,0 +1,35 @@
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+{
+    if(!root)
+      return;
+    else if(root->key == key)
+    {
+        if(root->left)
+        {
+            pre = root->left;
+            while(pre->right)
+            {
+                pre = pre->right;
+            }
+        }
+        if(root->right)
+        {
+            suc = root->right;
+            while(suc->left)
+            {
+                suc = suc->left;
+            }
+        }
+    }
+    else if(root->key > key)
+    {
+        suc = root;
+        findPreSuc(root->left, pre, suc, key);
+    }
+    else if(root->key < key)
+    {
+        pre = root;
+        findPreSuc(root->right, pre, suc, key);
+    }
+
+}
