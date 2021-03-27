c11ec94e82a4afe2f7493d2702c660b7007cf489
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-03-27 12:05:00 +0530

    gfg

--- ImplementTwoStacksInAnArray.cpp
+++ ImplementTwoStacksInAnArray.cpp
@@ -1,19 +1,13 @@
-/* The method push to push element into the stack 2 */
 void twoStacks :: push1(int x)
 {
     arr[++top1] = x;
 
 }
-   
-/* The method push to push element into the stack 2*/
 void twoStacks ::push2(int x)
 {
     arr[--top2] = x;
 
 }
-   
-/* The method pop to pop element from the stack 1 */
-//Return the popped element
 int twoStacks ::pop1()
 {
     if(top1 == -1)
@@ -21,9 +15,6 @@ int twoStacks ::pop1()
     return arr[top1--];
 
 }
-
-/* The method pop to pop element from the stack 2 */
-//Return the popped element
 int twoStacks :: pop2()
 {
     if(top2 == size)
