f356dc503c1605472302da55850d5e4ba9cede0f
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-04-15 00:51:44 +0530

    gfg

---
+++ AddOneToLL.cpp
@@ -0,0 +1,39 @@
+void reverse(Node *head)
+    {
+        Node *c = head;
+        Node *p;
+        
+        for(c=head; c != NULL; c=c->next)
+        {
+            for(p=c->next; p != NULL; p=p->next)
+            {
+                swap(c->data, p->data);
+            }
+        }
+        //return head;
+    }
+    Node* addOne(Node *head) 
+    {
+        Node *current;
+        reverse(head);
+        current = head;
+        if(current->data < 9)
+           current->data = current->data + 1;
+        else
+        {
+           current->data = current->data + 1;
+           while(current->next != NULL)
+           {
+               if(current->data == 10)
+               {
+                   current->data = 0;
+                   current->next->data = current->next->data + 1;
+                   current = current->next;
+               }
+               else
+                 break;
+           }
+        }
+        reverse(head);
+        return head;
+    }
