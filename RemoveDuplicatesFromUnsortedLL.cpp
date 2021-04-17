927412488f1ecb049cbbb1a46e67be09f498e70d
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-04-17 23:52:46 +0530

    gfg

--- RemoveDuplicatesFromUnsortedLL.cpp
+++ RemoveDuplicatesFromUnsortedLL.cpp
@@ -1,5 +1,29 @@
 //O(n^2) Approch
 
+#include<bits/stdc++.h>
+using namespace std;
+
+//Creation of Node class for Linked List
+class Node{
+public:
+	int data;
+	Node*next;
+	Node(int d){
+		data = d;
+		next = NULL;
+	}
+};
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = tem->next;
+    }
+}
+
 Node * removeDuplicates( Node *head) 
     {
         Node *current = head;
@@ -24,6 +48,39 @@ Node * removeDuplicates( Node *head)
      
     }
 
+    int main()
+    {
+        int T;
+        cin >> T;
+        for(int t=0;t<T;t++)
+        {
+            int n;
+            cin >> n;
+            struct Node *head = NULL;
+            struct Node *temp = head;
+
+            for(int i=0;i<n;i++)
+            {
+                int data;
+                cin >> data;
+                if(head == NULL)
+                {
+                    head = temp = new Node(data);
+                }
+                else
+                {
+                    temp->nxt = new Node(data);
+                    temp = temp->next;
+                }
+            }
+
+            Solution obj;
+            Node *res = obj.removeDuplicates(head);
+            print(res);
+            cout << endl;
+        }
+    }
+
     //O(n) approch
 
      Node * removeDuplicates( Node *head) 
@@ -36,10 +93,7 @@ Node * removeDuplicates( Node *head)
         {
             int value = current->data;
             if(st.find(value) != st.end())
-            {
-                
                 prev->next = current->next;
-            }
             else
             {
                 st.insert({value, i});
