dcd68b18b04036b44174645112171511b9fe0364
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-05-07 23:33:11 +0530

    gfg

--- coinChange.cpp
+++ coinChange.cpp
@@ -18,8 +18,7 @@
                dp[i][j] = dp[i-1][j];
                if(j-S[i-1] >= 0)
                   dp[i][j] += dp[i][j-S[i-1]];
-                
-           }
+            }
        }
        
        return dp[m][n];
