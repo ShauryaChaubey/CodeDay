d12092f162c885d7323078225c5212247e87d587
Shaurya Chaubey <shauryaccc98@gmail.com>
2021-07-14 00:52:37 +0530

    gfg

--- MatrixChainMultiplication.cpp
+++ MatrixChainMultiplication.cpp
@@ -20,3 +20,24 @@ int matrixMultiplication(int N, int arr[])
         }
         return dp[1][N-1];
     }
+
+    //top-down
+    int dp[101][101];
+    int solve(int arr[], int i, int j)
+    {
+        if(i >= j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        long long ans = INT_MAX;
+        for(int k=i; k<=j-1; k++)
+        {
+            long long temp = solve(arr, i, k) + solve(arr, k+1, j) + (arr[i-1]*arr[k]*arr[j]);
+            if(ans > temp) ans = temp;
+        }
+        
+        return dp[i][j] = ans;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        memset(dp, -1, sizeof(dp));
+        return solve(arr, 1, N-1);
+    }
---
+++ PalindromicPartitioning.cpp
@@ -0,0 +1,28 @@
+int dp[501][501];
+    bool isPalindrome(string &s, int i, int j)
+    {
+        while(i < j)
+        {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int solve(string &s, int i, int j)
+    {
+        if(i >= j || isPalindrome(s, i, j)) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = INT_MAX;
+        for(int k=i; k<j; k++)
+        {
+            int temp = solve(s, i, k) + solve(s, k+1, j) + 1;
+            ans = min(ans, temp);
+        }
+        return dp[i][j] = ans;
+    }
+    int palindromicPartition(string str)
+    {
+        memset(dp, -1, sizeof(dp));
+        return solve(str, 0, str.size()-1);
+    }
