a173f30f2710530402776cf0b57b7ae4cb511e6d
Shaurya Chaubey <shauryaccc98@gmail.com>
2022-03-31 22:30:03 +0530

    leetcode

--- DailyProblem_T5/Find the Duplicate Number.cpp
+++ DailyProblem_T5/Find the Duplicate Number.cpp
@@ -1,3 +1,8 @@
+//binary search is the best approch to solve such problems
+//count the numbers appearing from any one element, if count is greater than the actual no., there might be a repeatation
+//now check the smaller no., if the same case then keep going lower and find the lowest number having greater count than the no. itself. 
+
+
 int findDuplicate(vector<int>& nums) 
     {
         int l=1, h=nums.size()-1;
