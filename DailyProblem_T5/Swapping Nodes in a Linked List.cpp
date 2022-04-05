031b1dff4931e53849bbd94b7923e6ee0ec38d6a
Shaurya Chaubey <shauryaccc98@gmail.com>
2022-04-05 19:57:29 +0530

    leetcode

--- DailyProblem_T5/Swapping Nodes in a Linked List.cpp
+++ DailyProblem_T5/Swapping Nodes in a Linked List.cpp
@@ -33,3 +33,46 @@ ListNode* swapNodes(ListNode* head, int k)
         }
         return head;
     }
+
+//Using vector
+     ListNode* swapNodes(ListNode* head, int k) 
+    {
+        vector<int> v;
+        ListNode* current = head;
+        while(current)
+        {
+            v.push_back(current->val);
+            current = current->next;
+        }
+        swap(v[k-1], v[v.size()-k]);
+        current = head;
+        int i=0;
+        while(current)
+        {
+            current->val = v[i++];
+            current = current->next;
+        }
+        return head;
+    }
+
+
+//Two pointer approch
+    ListNode* swapNodes(ListNode* head, int k) 
+    {
+        ListNode* current = head;
+        ListNode* lt = head;
+        ListNode* rt = head;
+        int c=1;
+        while(current)
+        {
+            if(c < k) lt = lt->next;
+            if(c > k) rt = rt->next;
+            c++;
+            current = current->next;
+        }
+        cout << lt->val << " " << rt->val;
+        int temp = lt->val;
+        lt->val = rt->val;
+        rt->val = temp;
+        return head;
+    }
