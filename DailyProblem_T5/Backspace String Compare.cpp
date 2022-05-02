0245fa8c53ad1e2de5f9645f583a395d34d30a16
Shaurya Chaubey <shauryaccc98@gmail.com>
2022-05-02 22:58:16 +0530

    leetcode

--- DailyProblem_T5/Backspace String Compare.cpp
+++ DailyProblem_T5/Backspace String Compare.cpp
@@ -35,3 +35,32 @@ bool backspaceCompare(string s, string t)
         }
         return a==b?1:0;
     }
+
+    //stack based approch
+    bool backspaceCompare(string s, string t) 
+    {
+        stack<char> st;
+        string a="",b="";
+        for(int i=0; i<s.size(); i++)
+        {
+            if(!st.empty() && s[i] == '#') st.pop();
+            else if(s[i] != '#')st.push(s[i]);
+        }
+        while(!st.empty())
+        {
+           a += st.top();
+           st.pop();
+        }
+        for(int i=0; i<t.size(); i++)
+        {
+            if(!st.empty() && t[i] == '#') st.pop();
+            else if(t[i] != '#') st.push(t[i]);
+        }
+        while(!st.empty())
+        {
+           b += st.top();
+           st.pop();
+        }
+        if(a == b) return true;
+        return false;
+    }
