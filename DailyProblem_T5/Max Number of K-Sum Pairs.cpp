dd042b01bf488aa31cf6559ed3ca9183131ebc6a
Shaurya Chaubey <shauryaccc98@gmail.com>
2022-05-05 23:54:29 +0530

    leetcode

--- DailyProblem_T5/Max Number of K-Sum Pairs.cpp
+++ DailyProblem_T5/Max Number of K-Sum Pairs.cpp
@@ -15,3 +15,20 @@ int maxOperations(vector<int>& nums, int k)
         }
         return ans;
     }
+
+    int maxOperations(vector<int>& nums, int k) 
+    {
+        unordered_map<int, int> mp;
+        int ans=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(mp.find(k-nums[i]) != mp.end() && mp[k-nums[i]] > 0)
+            {
+                mp[k-nums[i]]--;
+                ans++;
+            }
+            else
+                mp[nums[i]]++;
+        }
+        return ans;
+    }
